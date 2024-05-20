bbd9e9a58daefae1f0e300a4428269cbc3702326
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-20 20:18:25 -0300

    atividade QUASE concluida

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿int number;
+
+Console.WriteLine("---->ATIVIDADE DE 'ENTRADA VALIDA<----/n");
+InsertNumber();
+
+if (number ==0 )
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Operação cancelada :(");
+    Console.ResetColor();
+}
+
+else
+{
+    if (number <=9 )
+    {
+        Console.WriteLine("Volte sempre! :)");
+    }
+
+    else
+    {
+        InsertNumber();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+void InsertNumber()
+{
+    Console.Write("Digite um número entre 1 e 9 (ou 0 para cancelar)...:");
+    number = Convert.ToInt32(Console.ReadLine());
+}
